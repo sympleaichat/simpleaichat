3e8abc2cd7f13f87200db076bfd2325d4cf118bf
sympleaichat <sympleaichat125@gmail.com>
2025-04-29 11:30:17 +0900

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static String apiKey = 'AIzaSyCSvGPxrvMpurmKjck6cOgVluJQFf5Nb-A'; // 最初は空、後で設定
+  static String apiKey = 'youre api key'; // 最初は空、後で設定
   static const String apiUrl =
       'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent';
 }
